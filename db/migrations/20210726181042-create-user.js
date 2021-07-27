2e02854d196d5cdddde7f4a37fee522b264e0358
walker <walkerwilliamsx@gmail.com>
2021-07-27 12:46:21 -0500

    Format

--- db/migrations/20210726181042-create-user.js
+++ db/migrations/20210726181042-create-user.js
@@ -37,12 +37,12 @@ module.exports = {
 			createdAt: {
 				allowNull: false,
 				type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('now')
+				defaultValue: Sequelize.fn('now'),
 			},
 			updatedAt: {
 				allowNull: false,
 				type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('now')
+				defaultValue: Sequelize.fn('now'),
 			},
 		});
 	},
