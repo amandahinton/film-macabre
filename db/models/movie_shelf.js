4157328912367ede2fe44d52ca1c1fc3b84501df
walker <walkerwilliamsx@gmail.com>
2021-07-27 12:00:00 -0500

    Added database code

--- db/models/movie_shelf.js
+++ db/models/movie_shelf.js
@@ -1,11 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Movie_shelf = sequelize.define('Movie_shelf', {
-    shelfId: DataTypes.INTEGER,
-    movieId: DataTypes.INTEGER
-  }, {});
-  Movie_shelf.associate = function(models) {
-    // associations can be defined here
-  };
-  return Movie_shelf;
+	const Movie_shelf = sequelize.define(
+		'Movie_shelf',
+		{
+			shelfId: DataTypes.INTEGER,
+			movieId: DataTypes.INTEGER,
+		},
+		{}
+	);
+	Movie_shelf.associate = function (models) {
+		// associations can be defined here
+	};
+	return Movie_shelf;
 };
