9363d618e4714a1df77e3318ad0f3fbb8858c174
tannerpedretti <tannerpedretti@gmail.com>
2021-07-27 17:27:58 -0400

    first iteration of users

---
+++ review-templates.js
@@ -0,0 +1,100 @@
+//this is a list of reviews that we can use as filler text
+
+let reviews = [
+    {
+        body:"This was a quality film. It was not perfect, but overall it fleshed out some new ideas and kept me interested",
+        rating:4,
+        userId:1,
+        movieId:1
+    },     
+    {
+        body:"Excellent movie! One of the best I have seen recently",
+        rating:5,
+        userId:2,
+        movieId:2
+    },    
+    {
+        body:"Decent. I went into this with higher expectations than I should have",
+        rating:3,
+        userId:3,
+        movieId:3
+    },
+    {
+        body:"The only reason this didn't get 1 star was because they killed off the annoying character quickly",
+        rating:2,
+        userId:4,
+        movieId:4
+    },
+    {
+        body:"One of the worst horror movies I have ever watched. Stay wway if you value your time.",
+        rating:1,
+        userId:5,
+        movieId:5
+    },
+    {
+        body:"Awful experience. The plot did not even make sense",
+        rating:1,
+        userId:2,
+        movieId:6
+    },
+    {
+        body:"I would have given this one star, but I have a huge crush on the lead actor. Not their best work at all",
+        rating:2,
+        userId:3,
+        movieId:7
+    },
+    {
+        body:"The premise and idea was good, but what the heck was that ending?!",
+        rating:3,
+        userId:4,
+        movieId:8
+    },
+    {
+        body:"Excellent film, although I thought that it ended a little too suddenly.",
+        rating:4,
+        userId:5,
+        movieId:9
+    },    
+    {
+        body:"Prepare to be absolutely terrified! What an experieence",
+        rating:5,
+        userId:2,
+        movieId:10
+    },
+    {
+        body:"I don't know why I even wasted my time with this. Do yourself a favor and skip it",
+        rating:1,
+        userId:3,
+        movieId:11
+    },
+    {
+        body:"Props for a scary monster/villian. Everything else was terribly executed.",
+        rating:2,
+        userId:4,
+        movieId:12
+    },
+    {
+        body:"You can tell they did not have a massivebudget to work with, but overall the film was pretty entertaining",
+        rating:3,
+        userId:5,
+        movieId:13
+    },    
+    {
+        body:"This was a great film! Although I would have liked to see a little more originality",
+        rating:4,
+        userId:2,
+        movieId:14
+    },   
+    {
+        body:"One of the best movie nights I can remember!! So scary! and it was an absolute blast to watch with my friends",
+        rating:5,
+        userId:3,
+        movieId:15
+    },   
+    {
+        body:"Pretty great! A few questionable character decisions, but a very enjoyable and fun film to watch with your partner ",
+        rating:4,
+        userId:4,
+        movieId:16
+    }
+]
