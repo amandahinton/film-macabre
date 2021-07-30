623b971b06c0f3baa63cf8b4c98856a5654531d0
tannerpedretti <tannerpedretti@gmail.com>
2021-07-30 17:27:23 -0400

    huhu

--- routes/users.js
+++ routes/users.js
@@ -106,6 +106,7 @@ router.get(
 			csrfToken: req.csrfToken(),
 			title: 'Register',
 			user,
+			content_container:"hearts",
 		});
 	})
 );
@@ -165,7 +166,7 @@ router.post(
 );
 
 router.get('/login', csrfProtection, (req, res) => {
-	res.render('login', { title: 'Login', csrfToken: req.csrfToken() });
+	res.render('login', { title: 'Login', csrfToken: req.csrfToken(), content_container:"grave" });
 });
 
 router.post('/logout', (req, res) => {
